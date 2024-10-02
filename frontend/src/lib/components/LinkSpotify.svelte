76be593483c9a0070545ba00cfd8a9adc83a0515
notsoli <samrandaofficial@gmail.com>
2024-10-02 14:19:17 -0400

    Created component from link Spotify button

---
+++ frontend/src/lib/components/LinkSpotify.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { goto } from '$app/navigation';
+    import spotify_logo from '$lib/assets/images/spotify_logo.png'
+</script>
+
+<button class="link_spotify" on:click={() => {goto('/link-spotify?link=true')}}>log in with Spotify<img class="spotify_logo" src={spotify_logo} alt="The Spotify logo."></button>
+
+<style>
+    .spotify_logo {
+        width: 30px;
+        height: 30px;
+    }
+
+    button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        border: none;
+        cursor: pointer;
+        color: var(--background);
+        font-family: "Mattone", sans-serif;
+        font-size: 15px;
+        height: 50px;
+        width: 300px;
+        border-radius: 10px;
+        background-color: #1DB954; /* spotify green */
+    }
+</style>
