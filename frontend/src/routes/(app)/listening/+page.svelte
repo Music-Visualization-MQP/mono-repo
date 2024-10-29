cc38de3e800040c0408da8a07fa8a7520dc82567
michibatoryu <114957192+michibatoryu@users.noreply.github.com>
2024-10-29 11:45:52 -0400

    Mich experimentation: Added onclick modal for listening data albums

--- frontend/src/routes/(app)/listening/+page.svelte
+++ frontend/src/routes/(app)/listening/+page.svelte
@@ -2,18 +2,39 @@
     import type { PageData } from './$types';
     export let data: PageData;
     import Song from './Song.svelte';
+    import Modal from './Modal.svelte';
+
+    let curAlbum:string
+    let showModal = false;
+    const openModal = (album:string) => {
+        showModal = true;
+        curAlbum = album
+    };
+
+    const closeModal = () => {
+        showModal = false;
+    };
 </script>
 
 <h1>Listening Data</h1>
 <div class="songs">
     {#if data.songs != null}
         {#each data.songs as song}
-            <Song {song}/>
+            <Song {song}  popup={()=> openModal(song.album)}/>
         {/each}
     {:else}
         <p>No listening data yet!</p>
     {/if}
 </div>
+
+<Modal isOpen={showModal} onClose={closeModal} album={curAlbum}>
+    <!-- <h1>Details for {curAlbum}
+    </h1>
+    
+    <div class="test">
+        hello!!!!! {curAlbum}
+    </div> -->
+  </Modal>
 
 <style>
     h1 {
---
+++ frontend/src/routes/(app)/listening/Modal.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    // export let showModal:boolean
+    export let isOpen = false;
+    export let album;
+    export let onClose;
+  
+    const closeModal = () => {
+        if (onClose) onClose();
+    }
+</script>
+
+{#if isOpen}
+    <div class="modal-overlay" on:click={closeModal}>
+        <div class="modal-content" on:click|stopPropagation>
+            <h1>Details for {album}
+            </h1>
+            <slot></slot>
+            <!-- <button class="close-btn" on:click={closeModal}>Close</button> -->
+        </div>
+    </div>
+{/if}
+
+<style>
+    .modal-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+  
+    .modal-content {
+        display: flex;
+        background: rgb(73, 70, 70);
+        padding: 1rem;
+        border-radius: 8px;
+        position: relative;
+        max-width: 75%;
+        width: 100%;
+    }
+  
+    /* .close-btn {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+    } */
+  </style>
